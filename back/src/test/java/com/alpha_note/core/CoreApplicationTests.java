517ddd86666dc61a485dd76cbd9a17dc9cef3e58
wonguwon25 <wonguwon25@gmail.com>
2025-08-08 17:42:11 +0900

    back init

---
+++ back/src/test/java/com/alpha_note/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha_note.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
